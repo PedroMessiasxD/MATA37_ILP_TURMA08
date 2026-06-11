c627fb605f022347b4093a13ab8a5696460d852a
edduarod <brandaoedd@gmail.com>
2026-06-11 01:18:26 -0300

    adicao de atividade

---
+++ Squad 03 - Redes e Caminhos/EduardoBrandão/atividade2/src/Main.java
@@ -0,0 +1,8 @@
+public class Main{
+    static int somaArray(int[] array){
+        int soma = 0;
+        for (int i = 0; i < array.length; i++){
+            soma += array[i];
+        }
+        return soma;
+    }
