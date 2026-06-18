e5006f3d747cb52ee6e9ba437d0709c92d49d333
cauaalmeida9 <caua.l.almeida@ba.estudante.senai.br>
2026-06-18 20:20:16 -0300

    Corrige atividade01

--- Squad 02 - Estruturas de Árvore/Caua Lucas Costa/atividade01.c
+++ Squad 02 - Estruturas de Árvore/Caua Lucas Costa/atividade01.c
@@ -1,20 +1,25 @@
 #include <stdio.h>
 
-int main(){
-    
+// Função que realiza a soma
+int soma(int a, int b) {
+    return a + b;
+}
+int main() {
+
     int a = 0;
     int b = 0;
-    int soma = 0;
+    int resultado = 0;
 
     scanf("%d", &a);
     scanf("%d", &b);
+
     if (a >= 1 && a <= 1000 && b >= 1 && b <= 1000) {
 
-        soma = a + b;
-        printf("%d", soma);
-    } else {
-        printf("Digite um numero de 1-100");
-    }
+        resultado = soma(a, b);
+        printf("%d", resultado);
 
+    } else {
+        printf("Digite um numero de 1 a 1000");
+    }
     return 0;
 }
