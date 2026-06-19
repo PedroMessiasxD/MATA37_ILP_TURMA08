37ebddcec9d1d0527f30f2a5445dd983580a6d7a
camilavalesca <153387397+camilavalesca@users.noreply.github.com>
2026-06-18 21:17:32 -0300

    Update Atividade03.py

--- Squad 01 - Grafos/CamilaValesca/Atividade03.py
+++ Squad 01 - Grafos/CamilaValesca/Atividade03.py
@@ -2,7 +2,6 @@ def proxima_maior_palavra(w):
     letras = list(w)
     n = len(letras)
     
-    # 1. Encontrar o ponto de quebra
     i = n - 2
     while i >= 0 and letras[i] >= letras[i + 1]:
         i -= 1
@@ -10,17 +9,14 @@ def proxima_maior_palavra(w):
     if i == -1:
         return "no answer"
         
-    # 2. Encontrar o substituto
     j = n - 1
     while letras[j] <= letras[i]:
         j -= 1
         
-    # 3. Trocar
     temp = letras[i]
     letras[i] = letras[j]
     letras[j] = temp
     
-    # 4. Inverter o sufixo
     esquerda = i + 1
     direita = n - 1
     while esquerda < direita:
@@ -30,7 +26,6 @@ def proxima_maior_palavra(w):
         esquerda += 1
         direita -= 1
         
-    # Reconstruir a string manualmente
     palavra_final = ""
     for char in letras:
         palavra_final += char
