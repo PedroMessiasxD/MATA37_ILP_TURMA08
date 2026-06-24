c476eb17d0cb0b9fa21939a5a0ce0e69830620b5
thamileit40 <thami7050@gmail.com>
2026-06-24 17:33:08 -0300

    Create Atividade1.py

---
+++ Squad 02 - Estruturas de Árvore/Thaimara_Leitao_da_Silva/Atividade1.py
@@ -0,0 +1,15 @@
+# Função que recebe dois números inteiros
+def soma(a, b):
+
+    # Somamos os dois valores recebidos
+    resultado = a + b
+
+    # Retornamos o resultado da soma
+    return resultado
+
+
+# Exemplo de uso
+a = 5
+b = 4
+
+print(soma(a, b))
