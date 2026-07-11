b15d851f771ee7c07ca973af5b05f1508243cc88
edduarod <brandaoedd@gmail.com>
2026-07-11 15:13:32 -0300

    adiciona atividades

--- Squad 03 - Redes e Caminhos/EduardoBrandão/atividade1/src/Main.java
+++
@@ -1,7 +0,0 @@
-import java.net.SocketAddress;
-public class Main {
-    public static int Somar(int a, int b) {
-        int Soma = a + b;
-        return Soma;
-    }
-}
---
+++ Squad 03 - Redes e Caminhos/EduardoBrandão/atividade2.py
@@ -0,0 +1,5 @@
+def simpleArraySum(ar):
+    total = 0
+    for i in range(len(ar)):
+        total = total + ar[i]
+    return total
